1275fab737ea3fcacbe9afc076ac9f1d96e041d2
LuaGanrqunx <aqunxgm@gmail.com>
2019-12-21 16:13:18 +0800

    update t006.sh

--- bin/t006.sh
+++ bin/t006.sh
@@ -3,9 +3,15 @@
 #先計算時間
 STARTTIME=$(date +%s)
 echo ${STARTTIME}
-#先用這個來模擬
-#sleep 10
-sudo tar cvpf /media/test/f540526c-212c-49d4-88d7-9af72f03e27c/backup.tar -X t006.dat /
+#使用t006.dat當排除清單
+#考慮：確認以下事項
+#使用媒體
+/*
+流程：
+掛載媒體
+*/
+sudo tar cvpf /media/test/f540526c-212c-49d4-88d7-9af72f03e27c/backup.tar \
+	-X t006.dat /
 ENDTIME=$(date +%s)
 echo ${ENDTIME}
 echo $[${ENDTIME} - ${STARTTIME}]
